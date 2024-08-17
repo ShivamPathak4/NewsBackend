843248d4279283ffa43ecb596b12b65204e6b73d
ShivamPathak4 <141670746+ShivamPathak4@users.noreply.github.com>
2024-08-17 06:51:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const {connectDB} = require('./config/db');
 // const cors = require("cors");
 const app = express();
 app.use(cors());
